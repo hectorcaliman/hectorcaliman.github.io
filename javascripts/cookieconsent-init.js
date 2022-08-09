0a9b7602706ee390603327a500fc58784e3804e8
hectorcaliman <56455384+hectorcaliman@users.noreply.github.com>
2022-08-09 11:41:46 -0300

    Update cookieconsent-init.js

--- javascripts/cookieconsent-init.js
+++ javascripts/cookieconsent-init.js
@@ -6,34 +6,34 @@ window.addEventListener('load', function(){
     // run plugin with your configuration
     cc.run({
         current_lang: 'pt-br',
-        autoclear_cookies: true,                   // default: false
-        page_scripts: true,                        // default: false
-        // mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
-        // delay: 80,                              // default: 0
-        auto_language: 'browser',                  // default: null; could also be 'browser' or 'document'
-        // autorun: true,                          // default: true
-        force_consent: false,                      // default: false
-        // hide_from_bots: false,                  // default: false
-        // remove_cookie_tables: false             // default: false
-        // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
-        // cookie_expiration: 182,                 // default: 182 (days)
-        // cookie_necessary_only_expiration: 182   // default: disabled
-        // cookie_domain: location.hostname,       // default: current domain
-        // cookie_path: '/',                       // default: root
-        // cookie_same_site: 'Lax',                // default: 'Lax'
-        // use_rfc_cookie: false,                  // default: false
-        // revision: 0,                            // default: 0
+        autoclear_cookies: true,
+        page_scripts: true,
+        // mode: 'opt-in'
+        // delay: 80,
+        auto_language: 'browser',
+        // autorun: true,
+        force_consent: false,
+        // hide_from_bots: false,
+        // remove_cookie_tables: false
+        // cookie_name: 'cc_cookie', 
+        // cookie_expiration: 182,
+        // cookie_necessary_only_expiration: 182
+        // cookie_domain: location.hostname,
+        // cookie_path: '/', 
+        // cookie_same_site: 'Lax',  
+        // use_rfc_cookie: false,   
+        // revision: 0, 
 
         onFirstAction: function(user_preferences, cookie){
-            // callback triggered only once on the first accept/reject action
+            
         },
 
        onAccept: function (cookie) {
-            // callback triggered on the first accept/reject action, and after each page load
+            
             },
 
         onChange: function (cookie, changed_categories) {
-            // callback triggered when user changes preferences after consent has already been given
+         
         },
 
         languages: {
@@ -43,11 +43,11 @@ window.addEventListener('load', function(){
                     description: 'Este site usa cookies para garantir que você obtenha a melhor experiência de navegação. Desativar os cookies do site pode prejudicar a funcionalidade de alguns recursos. <button type="button" data-cc="c-settings" class="cc-link">Quero escolher</button>',
                     primary_btn: {
                         text: 'Aceitar todos',
-                        role: 'accept_all'              // 'accept_selected' or 'accept_all'
+                        role: 'accept_all'
                     },
                     secondary_btn: {
                         text: 'Aceitar somente os necessários',
-                        role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+                        role: 'accept_necessary'
                     }
                 },
                 settings_modal: {
@@ -72,35 +72,49 @@ window.addEventListener('load', function(){
                             toggle: {
                                 value: 'necessary',
                                 enabled: true,
-                                readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+                                readonly: true     
                             },
-                            cookie_table: [             // list of all expected cookies
+                            cookie_table: [ 
                                 {
-                                    col1: '^_gtm',       // match all cookies starting with "_ga"
+                                    col1: '^_gtm',
                                     col2: 'google.com',
                                     col3: 'Indeterminado',
                                     col4: 'Gerenciador de Tags do Google',
                                     is_regex: true
                                 },
+                                {
+                                    col1: 'javascript',
+                                    col2: 'hectorcaliman.github.io',
+                                    col3: 'Indeterminado',
+                                    col4: 'Scripts de carregamento do site',
+                                    is_regex: true
+                                }
                             ]
                         }, {
                             title: 'Cookies de Performance e Analíticos',
                             description: 'Esses Cookies coletam informações e monitoram as ações dentro do site, cliques, páginas visitadas. Eles não serão usados para identificar quem acessou e todo o acesso será anônimo.',
                             toggle: {
-                                value: 'analytics',     // your cookie category
+                                value: 'analytics',
                                 enabled: false,
                                 readonly: false
                             },
-                            cookie_table: [             // list of all expected cookies
+                            cookie_table: [
                                 {
-                                    col1: '^_ga',       // match all cookies starting with "_ga"
+                                    col1: '^_ga',
                                     col2: 'google.com',
                                     col3: '5 anos',
-                                    col4: 'Gerenciador de Tags do Google',
+                                    col4: 'Configuração do Google Analytics Universal',
                                     is_regex: true
                                 },
                                 {
-                                    col1: '_gid',
+                                    col1: 'ˆ_gaawc',
+                                    col2: 'google.com',
+                                    col3: '180 dias',
+                                    col4: 'Configuração do Google Analytics 4',
+                                    is_regex: true
+                                },
+                                {
+                                    col1: 'ˆ_gid',
                                     col2: 'smartlook.com',
                                     col3: '180 dias',
                                     col4: 'Mapa de calor do site.',
@@ -117,19 +131,27 @@ window.addEventListener('load', function(){
                             },
                             cookie_table: [
                             {
-                                col1: '^_fb',               // New option in v2.4: regex (microsoft clarity cookies)
+                                col1: '^_fb',
                                 col2: 'facebook.com',
                                 col3: '365 dias',
                                 col4: 'Pixel de análise de eventos do Facebook',
-                                is_regex: true              // New option in v2.4
+                                is_regex: true 
                             },
                             {
-                                col1: '^_aw',               // New option in v2.4: regex (microsoft clarity cookies)
+                                col1: '^_sp',
+                                col2: 'google.com',
+                                col3: '180 dias',
+                                col4: 'Google Ads Remarketing Tag',
+                                // path: '/',
+                                is_regex: true
+                            },
+                            {
+                                col1: '^_aw',
                                 col2: 'google.com',
                                 col3: '180 dias',
                                 col4: 'Tag de conversão do Google Ads',
-                                // path: '/',               // New option in v2.4
-                                is_regex: true              // New option in v2.4
+                                // path: '/',
+                                is_regex: true
                             }
                             ]
                         }, {
