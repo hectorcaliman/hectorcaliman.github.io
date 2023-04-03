b17903a8ab35cf739c6eb83553a3274e837ee5b4
hectorcaliman <56455384+hectorcaliman@users.noreply.github.com>
2023-04-03 19:16:51 -0300

    Add files via upload

---
+++ viagem/javascripts/countdown.ready.js
@@ -0,0 +1,22 @@
+;(function($) {
+
+    'use strict'
+
+    var countDown = function() {
+        var before = '<div class="square"><div class="numb">',
+            text = '</div><div class="text">';
+                if ($().countdown) {
+                    $(".countdown").countdown('2023/5/09', function(event) {
+                $(this).html(event.strftime(before + '%D' + text + 'Dias</div></div>' + before + '%H' + text + 'Horas</div></div>' + before + '%M' + text + 'Minutos</div></div>' + before + '%S' + text + 'Segundos</div>'));
+                });
+            }         
+    };
+
+  	// Dom Ready
+      $(function() {
+        countDown();
+         });
+  })(jQuery);
+
+
+  
