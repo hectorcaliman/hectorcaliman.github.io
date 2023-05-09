af9e6fd06a52e7f5f55446a90e3daa1f8e8f9d1d
hectorcaliman <56455384+hectorcaliman@users.noreply.github.com>
2023-05-09 07:12:18 -0300

    countdown.ready.js

--- viagem/javascripts/countdown.ready.js
+++ viagem/javascripts/countdown.ready.js
@@ -6,7 +6,7 @@
         var before = '<div class="square"><div class="numb">',
             text = '</div><div class="text">';
                 if ($().countdown) {
-                    $(".countdown").countdown('2023/5/09', function(event) {
+                    $(".countdown").countdown('2023/6/01', function(event) {
                 $(this).html(event.strftime(before + '%D' + text + 'Dias</div></div>' + before + '%H' + text + 'Horas</div></div>' + before + '%M' + text + 'Minutos</div></div>' + before + '%S' + text + 'Segundos</div>'));
                 });
             }         
